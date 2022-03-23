1495a764ab145d01c59ad539647e2d93e56eb492
andririzki <andri.rizki007@gmail.com>
2022-03-23 15:18:31 +0700

    file

---
+++ index.php
@@ -0,0 +1,321 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <!-- basic -->
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <!-- mobile metas -->
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="viewport" content="initial-scale=1, maximum-scale=1">
+      <!-- site metas -->
+      <title>Pixarts</title>
+      <meta name="keywords" content="">
+      <meta name="description" content="">
+      <meta name="author" content="">
+      <!-- bootstrap css -->
+      <link rel="stylesheet" href="css/bootstrap.min.css">
+      <!-- style css -->
+      <link rel="stylesheet" href="css/style.css">
+      <!-- Responsive-->
+      <link rel="stylesheet" href="css/responsive.css">
+      <!-- fevicon -->
+      <link rel="icon" href="images/fevicon.png" type="image/gif" />
+      <!-- Scrollbar Custom CSS -->
+      <link rel="stylesheet" href="css/jquery.mCustomScrollbar.min.css">
+      <!-- Tweaks for older IEs-->
+      <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css" media="screen">
+      <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
+   </head>
+   <!-- body -->
+   <body class="main-layout">
+      <!-- loader  -->
+      <div class="loader_bg">
+         <div class="loader"><img src="images/loading.gif" alt="#" /></div>
+      </div>
+      <!-- end loader -->
+      <!-- header -->
+      <header>
+         <!-- header inner -->
+         <div  class="head_top">
+            <div class="header">
+               <div class="container-fluid">
+                  <div class="row">
+                     <div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col logo_section">
+                        <div class="full">
+                           <div class="center-desk">
+                              <div class="logo">
+                                 <a href="index.html"><img src="images/logo.png" alt="#" /></a>
+                              </div>
+                           </div>
+                        </div>
+                     </div>
+                     <div class="col-xl-9 col-lg-9 col-md-9 col-sm-9">
+                        <nav class="navigation navbar navbar-expand-md navbar-dark ">
+                           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
+                           <span class="navbar-toggler-icon"></span>
+                           </button>
+                           <div class="collapse navbar-collapse" id="navbarsExample04">
+                              <ul class="navbar-nav mr-auto">
+                                 <li class="nav-item">
+                                    <a class="nav-link" href="#">Home</a>
+                                 </li>
+                                 <!-- <li class="nav-item">
+                                    <a class="nav-link" href="#">About</a>
+                                 </li>
+                                 <li class="nav-item">
+                                    <a class="nav-link" href="#">Contact</a>
+                                 </li> -->
+                              </ul>
+                           </div>
+                        </nav>
+                     </div>
+                  </div>
+               </div>
+            </div>
+            <!-- end header inner -->
+            <!-- end header -->
+            <!-- banner -->
+            <section class="banner_main">
+               <div class="container-fluid">
+                  <div class="row d_flex">
+                     <div class="col-md-6">
+                        <div class="text-bg">
+                           <h1>Digital TopUp</h1>
+                           <p>Isi pulsa, paket data, bayar tagihan dan TopUp bisa langsung dengan sekali klik, mudah aman terpercaya</p>
+                           <a href="#">Read More</a>
+                        </div>
+                     </div>
+                     <div class="col-md-6">
+                        <div class="text-img">
+                           <figure><img src="images/box_img.png" alt="#"/></figure>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </section>
+         </div>
+      </header>
+      <!-- end banner -->
+      <!-- business -->
+      <div class="business">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-12">
+                  <div class="titlepage">
+                     <span>PIXARTS</span>
+                     <h2>Layanan PPOB</h2>
+                     <p>It is a long established fact that a reader will be distracted by the readable content of a page </p>
+                  </div>
+               </div>
+            </div>
+            <div class="row">
+               <div class="col-md-10 offset-md-1">
+                  <div class="row">
+                     <div class="col-md-12">
+                        <div class="business_box ">
+                           <figure><img src="images/business_img.jpg" alt="#"/></figure>
+                           <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believableThere are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable</p>
+                           <a class="read_more" href="#">Read more</a>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+      <!-- end business -->
+
+      <!-- Projects -->
+      <div class="projects">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-12">
+                  <div class="titlepage">
+                     <span>Produk</span>
+                     <h2>Tersedia dengan berbagai macam produk</h2>
+                     <p>Produk yang tersedia di jamin murah, aman dan terpercaya</p>
+                  </div>
+               </div>
+            </div>
+            <div>
+               <!-- SLIDER BLANTER -->
+               <div class='sliderbanner container'>
+                  <div class='owl-carousel owl-theme' id='blanter-carousel'>
+                     <!-- START -->
+                     <div class='blanter-owl-image'>
+                        <img src='images/produk/pixpulsa.png'/>
+                        <!-- <div style="background: url(images/produk/produk-1.jpg)"></div> -->
+                     </div>
+                     <div class='blanter-owl-image'>
+                        <img src='images/produk/produk-2.jpg'/>
+                        <!-- <div style="background: url(images/produk/produk-2.jpg)"></div> -->
+                     </div>
+                     <div class='blanter-owl-image'>
+                        <img src='images/produk/pixpulsa.png'/>
+                        <!-- <div style="background: url(images/produk/produk-1.jpg)"></div> -->
+                     </div>
+                     <div class='blanter-owl-image'>
+                        <img src='images/produk/produk-2.jpg'/>
+                        <!-- <div style="background: url(images/produk/produk-2.jpg)"></div> -->
+                     </div>
+                     <!-- END -->
+                  </div>
+               </div>
+               <!-- END SLIDER BLANTER -->
+            </div>
+         </div>
+      </div>
+      <!-- end projects -->
+
+      <!-- Testimonial -->
+      <!-- <div class="section">
+         <div class="container">
+            <div id="" class="Testimonial">
+               <div class="row">
+                  <div class="col-md-12">
+                     <div class="titlepage">
+                        <h2>Testimonial</h2>
+                     </div>
+                  </div>
+               </div>
+               <div class="row">
+                  <div class="col-md-3">
+                     <div class="Testimonial_box">
+                        <i><img src="images/plan1.png" alt="#"/></i>
+                     </div>
+                  </div>
+                  <div class="col-md-9">
+                     <div class="Testimonial_box">
+                        <h4>Donals</h4>
+                        <p>Tof Lorem Ipsum, you need to be There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a pass <br>
+                           age of Lorem Ipsum, you need to be 
+                        </p>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div> -->
+     
+      <!-- end Testimonial -->
+      <!-- contact -->
+      <div id="contact" class="contact">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-12">
+                  <div class="titlepage">
+                     <h2>Masukan</h2>
+                     <span>Masukan mu sangat berarti buat kami.</span>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div class="container">
+            <div class="row">
+               <div class="col-md-12 ">
+                  <form class="main_form ">
+                     <div class="row">
+                        <div class="col-md-12 ">
+                           <input class="form_contril" placeholder="Nama " type="text" name="nama">
+                        </div>
+                        <div class="col-md-12">
+                           <input class="form_contril" placeholder="Nomor HP" type="text" name="notelp">
+                        </div>
+                        <div class="col-md-12">
+                           <input class="form_contril" placeholder="Email" type="text" name="email">
+                        </div>
+                        <div class="col-md-12">
+                           <textarea class="textarea" placeholder="Catatan" type="text" name="catatan" rows="5"></textarea>
+                        </div>
+                        <div class="col-sm-12">
+                           <button class="send_btn">Kirim</button>
+                        </div>
+                     </div>
+                  </form>
+               </div>
+            </div>
+         </div>
+      </div>
+      <!-- end contact -->
+      <!--  footer -->
+      <footer>
+         <div class="footer">
+            <div class="container">
+               <div class="row">
+                  <div class="col-md-12 ">
+                     <div class="cont">
+                        <h3> <strong class="multi">Pixarts</strong><br>
+                        </h3>
+                     </div>
+                  </div>
+                  <div class="col-md-12">
+                     <ul class="social_icon">
+                        <div>
+                           <li><a href="#"><i class="fa fa-whatsapp" aria-hidden="true"></i></a></li>
+                           <li><a href="#"><i class="fa fa-telegram" aria-hidden="true"></i></i></a></li>
+                           <li><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></i></a></li>
+                        </div>
+                        <div>
+                           <a href="https://play.google.com/store" aria-hidden="true">
+                              <img src="images/play-store.png" alt="#" width="200"/>
+                           </a>
+                        </div>
+                     </ul>
+                  </div>
+               </div>
+            </div>
+            <div class="copyright">
+               <div class="container">
+                  <div class="row">
+                     <div class="col-md-12">
+                        <p>&#169; Reserved By <a href="<?= '#' ?>">Pixarts</a></p>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </footer>
+      <!-- end footer -->
+      <!-- Javascript files-->
+      <script src="js/jquery.min.js"></script>
+      <script src="js/popper.min.js"></script>
+      <script src="js/bootstrap.bundle.min.js"></script>
+      <script src="js/jquery-3.0.0.min.js"></script>
+      <script src="js/plugin.js"></script>
+      <!-- sidebar -->
+      <script src="js/jquery.mCustomScrollbar.concat.min.js"></script>
+      <script src="js/custom.js"></script>
+      <script src="https:cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.js"></script>
+      <script src="https://cdn.jsdelivr.net/gh/blanter/design@master/blanter-owlcarousel.js"></script>
+      <script>
+         $('.owl-carousel').owlCarousel({
+            loop: true,
+            center: true,
+            items: 3,
+            margin: 15,
+            autoplay: true,
+            dots: true,
+            nav: true,
+            autoplayTimeout: 8500,
+            smartSpeed: 450,
+            responsive: {
+               0: {
+                  items: 1
+               },
+               768: {
+                  items: 2
+               },
+               1170: {
+                  items: 3
+               }
+            }
+         });
+      </script>
+   </body>
+</html>
+
